de7ea776c2b81f4a5152ff1f55d989559c5a0d24
Matthew Kurtz <mkusy936@gmail.com>
2020-09-02 19:53:24 +0200

    added the basic functionality of mimes revealed when one mime is clicked

--- __main__.py
+++ __main__.py
@@ -3,9 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from src.gui.main_create_gui import mimesweeper_main_create_gui
 from src.classes.Tile import Tile
-
-import resources
-from random import randint
+from random import randint, choice
 
 
 class MimeSweeperMain(QMainWindow):
@@ -26,25 +24,30 @@ class MimeSweeperMain(QMainWindow):
         central_layout.setSpacing(0)
         self.mine_count = 0
         self.tile_list = []
+        self.mime_list = []
+        self.mime_number = 20
 
+        # generating tiles
         for x in range(10):
             for y in range(10):
                 tile = Tile(self)
                 central_layout.addWidget(tile, x, y, 1, 1)
-        # running check to make sure that the given number of mimes is always present
-        while self.mine_count < 10:
-            for tile in self.tile_list:
-                chance = randint(1, 10)
-                if chance == 1:
-                    tile.content = 'X'
-                    tile.setText('B')
-                    self.mine_count += 1
-                    tile.clicked.connect(lambda: tile.setText(tile.content))
+        # generating mimes
+        for x in range(self.mime_number):
+            chosen_tile = choice(self.tile_list)
+            chosen_tile.content = 'MIME'
+            chosen_tile.clicked.connect(self.mime_disturbed)
+            self.tile_list.remove(chosen_tile)
+            self.mime_list.append(chosen_tile)
         self.central_widget.setLayout(central_layout)
         self.setCentralWidget(self.central_widget)
         self.setFixedSize(self.size())
 
-
+    def mime_disturbed(self):
+        for mime in self.mime_list:
+            mime.setIcon(QIcon('://mime_face.png'))
+            mime.setIconSize(QSize(20, 20))
+            mime.setChecked(True)
 
 
 if __name__ == '__main__':
--- src/classes/Tile.py
+++ src/classes/Tile.py
@@ -16,10 +16,5 @@ class Tile(QPushButton):
         self.content = ''
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         window.tile_list.append(self)
-        if window.mine_count < 10:
-            chance = randint(1, 10)
-            if chance == 1:
-                self.content = 'X'
-                self.setText('B')
-                window.mine_count += 1
-                self.clicked.connect(lambda: self.setText(self.content))
+
+
