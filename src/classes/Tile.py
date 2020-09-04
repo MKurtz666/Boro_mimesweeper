9d99455b6637f84266e9ca0c203191d7a192117b
Matthew Kurtz <mkusy936@gmail.com>
2020-09-04 13:52:45 +0200

    removed the usage of exception handling as part of the logic by adding auxiliary function to check whether given index in range / removed the repeared .index() calls in both main_create_gui() and reveal_neighbouring_zeros()

--- src/classes/Tile.py
+++ src/classes/Tile.py
@@ -1,9 +1,7 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-
 import resources
-from random import randint
 
 
 class Tile(QPushButton):
@@ -16,7 +14,33 @@ class Tile(QPushButton):
         self.parent = window
         self.content = ''
         self.neighbouring_mimes = 0
+        self.caution_flag = False
         window.tile_list.append(self)
+
+    @staticmethod
+    def check_if_tile_in_range(matrix, row_index, tile_index_to_be_ckecked):
+        if tile_index_to_be_ckecked <= len(matrix[row_index]) - 1:
+            return True
+        return False
+
+    @staticmethod
+    def check_if_row_in_range(matrix, row_index_to_be_ckecked):
+        if row_index_to_be_ckecked <= len(matrix) - 1:
+            return True
+        return False
+
+    def mousePressEvent(self, QMouseEvent):
+        if QMouseEvent.button() == Qt.RightButton:
+            if not self.isChecked():
+                if self.caution_flag is False:
+                    self.setIcon(QIcon('://caution_icon.png'))
+                    self.setIconSize(QSize(20, 20))
+                    self.caution_flag = True
+                else:
+                    self.setIcon(QIcon())
+                    self.caution_flag = False
+        else:
+            self.reveal_tile()
 
     def reveal_tile(self):
         if self.content == 'MIME':
@@ -39,44 +63,32 @@ class Tile(QPushButton):
 
     def reveal_neighbouring_zeros(self, row_index, tile_index):
         # checking for tiles containing a number further down the row
-        try:
+        if self.check_if_tile_in_range(self.parent.matrix, row_index, tile_index + 1):
             if self.parent.matrix[row_index][tile_index + 1].content != 'MIME' and not \
                     self.parent.matrix[row_index][tile_index + 1].isChecked():
                 self.parent.matrix[row_index][tile_index + 1].reveal_tile()
-        except IndexError:
-            pass
         # checking for numbers in previous row, same tile index under the condition
         # that this is not the first row
-        try:
+        if self.check_if_row_in_range(self.parent.matrix, row_index - 1):
             if row_index != 0:
                 if self.parent.matrix[row_index - 1][tile_index].content != 'MIME' and not \
                         self.parent.matrix[row_index - 1][tile_index].isChecked():
                     self.parent.matrix[row_index - 1][tile_index].reveal_tile()
-        except IndexError:
-            pass
         # checking for numbers in the next row. No condition as index error handled
-        try:
+        if self.check_if_row_in_range(self.parent.matrix, row_index + 1):
             if self.parent.matrix[row_index + 1][tile_index].content != 'MIME' and not \
                     self.parent.matrix[row_index + 1][tile_index].isChecked():
                 self.parent.matrix[row_index + 1][tile_index].reveal_tile()
-        except IndexError:
-            pass
         # checking for numbers on the previous index in the row under the condition that
         # index of current tile not 0
-        try:
+        if self.check_if_tile_in_range(self.parent.matrix, row_index, tile_index - 1):
             if tile_index != 0:
                 if self.parent.matrix[row_index][tile_index - 1].content != 'MIME' and not \
                         self.parent.matrix[row_index][tile_index - 1].isChecked():
                     self.parent.matrix[row_index][tile_index - 1].reveal_tile()
-        except IndexError:
-            pass
 
-    def mousePressEvent(self, QMouseEvent):
-        if QMouseEvent.button() == Qt.RightButton:
-            self.setIcon(QIcon('://caution_icon.png'))
-            self.setIconSize(QSize(20, 20))
-        else:
-            self.reveal_tile()
+
+
 
 
 
--- src/gui/main_create_gui.py
+++ src/gui/main_create_gui.py
@@ -40,39 +40,59 @@ def generate_mimes(window):
             window.mimes_to_be_deployed -= 1
 
 
-def generate_clean_tiles(window):
-    matrix = window.matrix
-    for row in matrix:
-        for tile in row:
-            if tile.content != 'MIME':
-                try:
-                    if row[row.index(tile) + 1] in window.mime_list:
-                        tile.neighbouring_mimes += 1
-                except IndexError:
-                    pass
-                try:
-                    if row is not matrix[0]:
-                        if matrix[matrix.index(row) - 1][row.index(tile)] in window.mime_list:
-                            tile.neighbouring_mimes += 1
-                except IndexError:
-                    pass
-                try:
-                    if matrix[matrix.index(row) + 1][row.index(tile)] in window.mime_list:
-                        tile.neighbouring_mimes += 1
-                except IndexError:
-                    pass
-                try:
-                    if tile is not row[0]:
-                        if row[row.index(tile) - 1] in window.mime_list:
-                            tile.neighbouring_mimes += 1
-                except IndexError:
-                    pass
-
-                tile.content = str(tile.neighbouring_mimes)
-                tile.clicked.connect(tile.reveal_tile)
-
-
 def create_matrix(window):
     window.matrix = [[tile for tile in window.tile_list[window.tile_list.index(tile):window.tile_list.index(tile) + 10]]
                      for tile in window.tile_list[::10]]
+
+
+def check_if_tile_in_range(matrix, row_index, tile_index_to_be_ckecked):
+    if tile_index_to_be_ckecked <= len(matrix[row_index]) - 1:
+        return True
+    return False
+
+
+def check_if_row_in_range(matrix, row_index_to_be_ckecked):
+    if row_index_to_be_ckecked <= len(matrix) - 1:
+        return True
+    return False
+
+
+def generate_clean_tiles(window):
+    matrix = window.matrix
+    # iterating through every row in matrix
+    for row in matrix:
+        # iterating through every tile in row
+        for tile in row:
+            # executing if tile is not already a MIME tile
+            if tile.content != 'MIME':
+                # pinning down tile position in matrix
+                row_index = matrix.index(row)
+                tile_index = row.index(tile)
+                # checking if tile 'to the right' is in row range
+                if check_if_tile_in_range(matrix, row_index, tile_index + 1):
+                    # if tile 'to the right' in range adding to the count of neighbouring mimes
+                    if row[tile_index + 1] in window.mime_list:
+                        tile.neighbouring_mimes += 1
+                # checking if tile 'up' is in matrix range
+                if check_if_row_in_range(matrix, row_index - 1):
+                    # if tile 'up' in range and tile not in first row adding to count of neighboring mimes
+                    if row_index != 0:
+                        if matrix[row_index - 1][tile_index] in window.mime_list:
+                            tile.neighbouring_mimes += 1
+                # checking if tile 'down' in range of matrix
+                if check_if_row_in_range(matrix, row_index + 1):
+                    # if tile 'down' in range of matrix adding to the count of neighbouring mimes
+                    if matrix[row_index + 1][tile_index] in window.mime_list:
+                        tile.neighbouring_mimes += 1
+                # checking if tile 'to the left' is in row range
+                if check_if_tile_in_range(matrix, row_index, tile_index - 1):
+                    # if tile 'to the left' is in row range adding to the count of neighbouring mimes
+                    if tile_index != 0:
+                        if row[tile_index - 1] in window.mime_list:
+                            tile.neighbouring_mimes += 1
+                # setting the (yet invisible) tile content to reflect number of neighbor mimes
+                tile.content = str(tile.neighbouring_mimes)
+
+
+
 
